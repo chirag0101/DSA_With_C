428c33d70bee3a50208fe1e4e6b6d5df618d4b14
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-11 08:34:37 +0530

    trees

--- Trees/prac.c
+++ Trees/prac.c
@@ -1,251 +1,117 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-
-// typedef struct node{
-//     int data;
-//     struct node *left;
-//     struct node *right;
-// }n;
-
-// n* createNode(n *root,int level){
-//     level++;
-//     n *newNode=(n*)malloc(sizeof(n));
-    
-//     printf("Enter Data: ");
-//     scanf("%d",&newNode->data);
-    
-//     getchar();
-//     char ch;
-//     printf("\nDo u want to create left node for level %d ? ",level);
-//     scanf("%c",&ch);
-
-//     if(ch=='Y'||ch=='y'){
-//         newNode->left=createNode(root,level);
-//     }else{
-//         newNode->left=NULL;
-//     }
-
-//     getchar();
-//     printf("\nDo u want to create right node for level %d ? ",level);
-//     scanf("%c",&ch);
-
-//     if(ch=='Y'||ch=='y'){
-//         newNode->right=createNode(root,level);
-//     }else{
-//         newNode->right=NULL;
-//     }
-
-//     return newNode;
-// }
-// void preOrder(n *root){
-//     if(root==NULL){
-//         return ;
-//     }
-//     printf("%d ",root->data);
-//     preOrder(root->left);
-//     preOrder(root->right);
-// }
-
-// void main(){
-//     n *root=(n*)malloc(sizeof(n));
-//     printf("Enter data for root node: ");
-//     scanf("%d",&root->data);
-//     int level=0;
-//     getchar();
-//     char ch;
-//     printf("\nDo u want to create Left subtree? ");
-//     scanf("%c",&ch);
-//     if(ch=='Y'||ch=='y'){
-//         root->left=createNode(root,level);
-//     }
-
-//     getchar();
-//     printf("\nDo u want to create Right subtree? ");
-//     scanf("%c",&ch);
-//     if(ch=='Y'||ch=='y'){
-//         root->right=createNode(root,level);
-//     }
-
-//     preOrder(root);
-// }
-
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node{
+struct TreeNode{
     int data;
-    struct node *left;
-    struct node *right;
-}n;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
 
-n* solvePre(int in[],int pre[],int inStart,int inEnd,int preStart,int preEnd){
-    int rootData=pre[preStart];
-    if(inStart>inEnd){
-        return NULL;
+struct TreeNode* constructBt(struct TreeNode* root,int level){
+    level++;
+    struct TreeNode *newNode=malloc(sizeof(struct TreeNode));
+
+    printf("Enter Data:");
+    scanf("%d",&(newNode->data));
+
+    char ch;
+    getchar();
+    printf("Add left subtree for level %d :(Y/N)?",level);
+    scanf("%c",&ch);
+    if(ch=='N'||ch=='n'){
+        root->left=NULL;
+    }else{
+        root->left=constructBt(newNode,level);
     }
 
-    n *temp=(n*)malloc(sizeof(n));
-    temp->data=rootData;
-
-    if(inStart==inEnd){
-        return temp;
+    getchar();
+    printf("Add right subtree for level %d :(Y/N)?",level);
+    scanf("%c",&ch);
+    if(ch=='N'||ch=='n'){
+        root->right=NULL;
+    }else{
+        root->right=constructBt(newNode,level);
     }
+    return newNode;
+}
 
-    int idx;
-    for(idx=inStart;idx<=inEnd;idx++){
-        if(in[idx]==rootData){
+void preOrder(struct TreeNode *root){
+    if(root==NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(struct TreeNode *root){
+    if(root==NULL){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d",root->data);
+}
+
+void inOrder(struct TreeNode *root){
+    if(root==NULL){
+        return;
+    }
+    inOrder(root->left);
+    printf("%d ",root->data);
+    inOrder(root->right);
+}
+
+void printBtree(struct TreeNode *root){
+    char cho;
+    do{
+        int ch;
+        printf("1.Pre 2.Post 3.In: ");
+        scanf("%d",&ch);
+
+        switch (ch)
+        {
+        case 1: preOrder(root);
             break;
-        }
-    }
-
-    int Llen=idx-inStart;
-    temp->left=solvePre(in,pre,inStart,idx-1,preStart+1,preStart+Llen);
-    temp->right=solvePre(in,pre,idx+1,inEnd,preStart+Llen+1,preEnd);    
-    return temp;
-}
-
-n* solvePost(int in[],int post[],int inStart,int inEnd,int postStart,int postEnd){
-    int rootData=post[postEnd];
-    if(inStart>inEnd){
-        return NULL;
-    }
-
-    n *temp=(n*)malloc(sizeof(n));
-    temp->data=rootData;
-
-    if(inStart==inEnd){ 
-        return temp;
-    }
-
-    int idx;
-    for(idx=inStart;idx<=inEnd;idx++){
-        if(in[idx]==rootData){
+        case 2: postOrder(root);
             break;
+        case 3: inOrder(root);
+            break;  
         }
-    }
-
-    int Llen=idx-inStart;
-    temp->left = solvePost(in,post,inStart,idx-1,postStart,postStart+Llen-1);
-    temp->right = solvePost(in,post,idx+1,inEnd,postStart+Llen,postEnd-1);
-    return temp;
+        getchar();
+        printf("\nContinue?:(y/n):");
+        scanf("%c",&cho);
+    }while(cho=='Y'||cho=='y');
 }
 
-void preOrder(n *root){
-    if(root==NULL){
-        return;
+void main() {
+    struct TreeNode *root=NULL;
+
+    root=malloc(sizeof(struct TreeNode));
+
+    printf("Enter root data:");
+    scanf("%d",&(root->data));
+
+    char ch;
+    getchar();
+    printf("Add left subtree :(Y/N)?");
+    scanf("%c",&ch);
+
+    if(ch=='N'||ch=='n'){
+        root->left=NULL;
     }else{
-        printf("%d ",root->data);
-        preOrder(root->left);
-        preOrder(root->right);
+        root->left=constructBt(root,0);
     }
-}
 
-void inOrder(n *root){
-    if(root==NULL){
-        return;
+    getchar();
+    printf("Add right subtree :(Y/N)?");
+    scanf("%c",&ch);
+
+    if(ch=='N'||ch=='n'){
+        root->right=NULL;
     }else{
-        inOrder(root->left);
-        printf("%d ",root->data);
-        inOrder(root->right);
+        root->right=constructBt(root,0);
     }
-}
 
-void postOrder(n *root){
-    if(root==NULL){
-        return;
-    }else{
-        postOrder(root->left);
-        postOrder(root->right);
-        printf("%d ",root->data);
-    }
-}
-
-void main(){
-    int ch;    
-    printf("Options:");
-    printf("\n1.inOrder+preOrder->Tree");
-    printf("\n2.inOrder+postOrder->Tree");
-    printf("\n3.Exit");
-    printf("\nEnter Choice:");
-    scanf("%d",&ch);
-    
-    int size;
-    printf("\nEnter num of nodes:");
-    scanf("%d",&size);
-    
-    switch(ch){
-        case 1: {
-                    int in[size];
-                    printf("\nEnter inOrder:");
-                    for(int i=0;i<size;i++){
-                        scanf("%d",&in[i]);
-                    }
-                    int pre[size];
-                    printf("\nEnter preOrder:");
-                    for(int i=0;i<size;i++){
-                        scanf("%d",&pre[i]);
-                    }
-
-                    n *root=solvePre(in,pre,0,size-1,0,size-1);
-
-                    int choice;
-                    printf("\n1.PreOrder.");
-                    printf("\n2.InOrder.");
-                    printf("\n3.PostOrder.");
-                    printf("\n4.Exit.");
-                    printf("\nEnter Choice:");
-                    scanf("%d",&choice);
-    
-                    switch(choice){
-                        case 1: preOrder(root);
-                                    break;
-                        case 2: inOrder(root);
-                                    break;
-                        case 3: postOrder(root);
-                                    break;
-                        case 4: exit(0);
-                                    break;
-                        default: printf("\nInvalid Choice!");
-                    }
-                    break;
-                }
-        case 2:{
-                    int in[size];
-                    printf("\nEnter inOrder:");
-                    for(int i=0;i<size;i++){
-                        scanf("%d",&in[i]);
-                    }
-                    int post[size];
-                    printf("\nEnter postOrder:");
-                    for(int i=0;i<size;i++){
-                        scanf("%d",&post[i]);
-                    }
-
-                    n *root=solvePost(in,post,0,size-1,0,size-1);
-                    
-                    int choice;
-                    printf("\n1.PreOrder.");
-                    printf("\n2.InOrder.");
-                    printf("\n3.PostOrder.");
-                    printf("\n4.Exit.");
-                    printf("\nEnter Choice:");
-                    scanf("%d",&choice);
-    
-                    switch(choice){
-                        case 1: preOrder(root);
-                                    break;
-                        case 2: inOrder(root);
-                                    break;
-                        case 3: postOrder(root);
-                                    break;
-                        case 4: exit(0);
-                                    break;
-                        default: printf("\nInvalid Choice!");
-                    }
-                    break;
-        }
-        case 3: exit(0);
-                    break;
-        default: printf("\nInvalid Choice!");
-    }
+    printBtree(root);
 }
