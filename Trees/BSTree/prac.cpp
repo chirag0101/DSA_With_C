785e6f4ba1696b1a9700b68a5220421cdf6a122d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-12 18:48:13 +0530

    bst

--- Trees/BSTree/prac.cpp
+++ Trees/BSTree/prac.cpp
@@ -1,39 +1,12 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int size;
-    cout<<"Enter Size:";
-    cin>>size;
-
-    int arr[size];
-
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
+bool searchInBst(b* root,int data){
+    if(root==NULL){
+        return false;
     }
-
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+    if(root->data>data){
+        return searchInBst(root->left,data);
     }
-    
-
-    int ele;
-    cout<<"Enter ele:";
-    cin>>ele;
-
-    for(int i=0;i<size;i++){
-        if(arr[i]==ele){
-            for(int j=i;j<size-1;j++){
-                arr[j]=arr[j+1];
-            }
-            size--;
-        }
+    if(root->data<data){
+        return searchInBst(root->right,data);
     }
-
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
-    
-
-    return 0;
+    return true;
 }
