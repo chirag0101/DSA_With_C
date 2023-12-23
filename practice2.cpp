a2227302ab6a9fcfa6185d4fe46a822fb9441e7d
chirag <chiragsuryavanshi01@gmail.com>
2023-12-23 22:55:08 +0530

    uniqueElement

---
+++ practice2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int ch=2;
+
+    switch(ch){
+        case 1:
+            cout<<"hello";
+            continue;
+        case 2:
+            cout<<"ok";
+            break;
+    }
+    return 0;
+}
