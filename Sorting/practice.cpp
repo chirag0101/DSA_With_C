9974b8090d025efe2d155982996f6b25a47759ea
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-01-10 22:51:05 +0530

    practice.cpp

--- Sorting/practice.cpp
+++ Sorting/practice.cpp
@@ -2,19 +2,29 @@
 using namespace std;
 
 int main(){
-    int arr[]={5,4,3,2,1};
+    int size;
+    cout<<"Enter Size:";
+    cin>>size;
 
-    for(int i=0;i<5;i++){
-        int small=arr[i];
-        for(int j=i+1;j<5;j++){
-            if(arr[j]<small){
+    int arr[size];
+
+    cout<<"Enter Elements:";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<size;i++){
+        int min=arr[i];
+        for(int j=i+1;j<size;j++){
+            if(min>arr[j]){
                 swap(arr[i],arr[j]);
             }
         }
     }
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<size;i++){
         cout<<arr[i];
     }
+
     return 0;
 }
