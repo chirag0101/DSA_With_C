858c8e57fc2e2ef28d739d942eca31e2b3b58f14
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-01-12 10:15:36 +0530

    practice

--- Sorting/practice.cpp
+++ Sorting/practice.cpp
@@ -2,30 +2,31 @@
 using namespace std;
 
 int main(){
-    int size;
-    cout<<"Enter Size:";
-    cin>>size;
-
-    int arr[size];
-
-    cout<<"Enter Elements:";
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-
-    for(int i=0;i<size;i++){
-        int min=arr[i];                         //6
-        for(int j=i+1;j<size;j++){
-            if(min>arr[j]){                     //6>2
-                swap(arr[i],arr[j]);
-                break;
-            }
+    int arr[]={6,5,4,3,2,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=0;i<size-1;i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
         }
+
+        int j=i;
+            while(arr[j]<arr[j-1] && j>0){
+                    swap(arr[j],arr[j-1]);
+                    j--;
+            }
+
+    for(int k=0;k<size;k++){
+        cout<<arr[k]<<" ";
+    }
+    cout<<"\n";
+
+
     }
 
-    for(int i=0;i<size;i++){
-        cout<<arr[i];
-    }
+    // for(int i=0;i<size;i++){
+    //     cout<<arr[i]<<" ";
+    // }
 
     return 0;
 }
